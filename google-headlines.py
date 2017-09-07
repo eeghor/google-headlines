fcf369037e7d9180bb5c2717bf08c5b8e69762a8
Unknown <eeghor@gmail.com>
2017-09-07 17:06:18 +1000

    further updates

--- google-headlines.py
+++ google-headlines.py
@@ -19,8 +19,24 @@ WebDriverWait(driver, WAIT_TIME).until(EC.presence_of_element_located((By.XPATH,
 driver.find_element_by_xpath("//input[@type='submit']").click()
 WebDriverWait(driver, WAIT_TIME).until(EC.presence_of_element_located((By.PARTIAL_LINK_TEXT, "News"))).click()
 
-for d in driver.find_elements_by_xpath("//div[contains(@class, 'Js')]"):
-	print(d.text)
+for d in driver.find_elements_by_xpath("//div[contains(@class, 'card-section')]"):
+	aas = d.find_elements_by_xpath(".//a")
+	if len(aas) == 1:
+		hdl = aas[-1].text.strip()
+		if len(hdl.split()) > 2:
+			print(hdl)
+			src = " ".join([w.text.strip() for w in d.find_elements_by_xpath(".//span")])
+			print(src)
+for h3 in driver.find_elements_by_xpath("//h3"):
+	hdl = h3.find_element_by_xpath(".//a").text.strip()
+	print(hdl)
+	src = h3.find_element_by_xpath(".//following-sibling::div").text.strip()
+	print(src)
+	#abst = d.find_element_by_xpath(".//div[@class='st']").text.strip()
+	
+	#print(d.text.strip())
+	#print("--- abstract:", abst)
+	#print("--- source:", src)
 
 
 
